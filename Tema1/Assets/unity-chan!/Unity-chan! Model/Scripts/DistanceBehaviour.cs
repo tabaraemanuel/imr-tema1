65fa671f410decae9fcacc6aaf15daecd71fac0f
tabaraemi <emanuel.tabara@info.uaic.ro>
2020-10-07 22:26:22 +0300

    First commit
    First commit

---
+++ Tema1/Assets/unity-chan!/Unity-chan! Model/Scripts/DistanceBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class DistanceBehaviour : MonoBehaviour
+{
+    float distance;
+    Animator miazaky;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        miazaky = GetComponent<Animator>();
+        distance = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xpos = Camera.main.transform.position.x;
+        float ypos = Camera.main.transform.position.y;
+        float x = transform.position.x;
+        float y = transform.position.y;
+         distance = Mathf.Sqrt(Mathf.Pow((xpos - x),2) + Mathf.Pow((ypos - y),2));
+         miazaky.SetFloat("Distance",distance);
+
+    }
+}
